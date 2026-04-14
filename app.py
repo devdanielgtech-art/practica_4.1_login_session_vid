124e38849b09c53cd2892347e26edadb32ebfd3d
devdanielgtech-art <devdanielgtech@gmail.com>
2026-04-14 12:50:57 -0300

    Corregido: agregados templates y arreglado app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,17 +8,16 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # verificar si esta logueado
-    usuario = request.form["usuario"]
-    password = request.form["password"]
-    return render_template("login.html", usuario=usuario, password=password)
-
-
-    if usuario == 'admin' and password == '123':
-        return render_template("dashboard.html", usuario=usuario)
-    else:
-        return render_template("index.html")
-
+    if request.method == "POST":
+        usuario = request.form["usuario"]
+        password = request.form["password"]
+        
+        if usuario == 'admin' and password == '123':
+            return render_template("dashboard.html", usuario=usuario)
+        else:
+            return render_template("index.html", error="Credenciales incorrectas")
+    
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
